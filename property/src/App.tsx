6bf98a130e7246a3a4486c8f30e1faa088712ade
justin725 <jleung@marketforce.com>
2024-02-13 21:32:29 +0000

    Kanban part 1

--- property/src/App.tsx
+++ property/src/App.tsx
@@ -1,4 +1,4 @@
-import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
+import { Authenticated,  Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -23,25 +23,20 @@ import { authProvider } from "./authProvider";
 import { Header } from "./components/header";
 import { ColorModeContextProvider } from "./contexts/color-mode";
 import {
-  BlogPostCreate,
-  BlogPostEdit,
-  BlogPostList,
-  BlogPostShow,
-} from "./pages/blog-posts";
-import {
-  CategoryCreate,
-  CategoryEdit,
-  CategoryList,
-  CategoryShow,
-} from "./pages/categories";
-import { ForgotPassword } from "./pages/forgotPassword";
-import { Login } from "./pages/login";
-import { Register } from "./pages/register";
+  AccountCircleOutlined,
+  ChatBubbleOutline,
+  PeopleAltOutlined,
+  StarOutlineRounded,
+  VillaOutlined,
+  DashboardOutlined
+} from '@mui/icons-material';
+
+import { Home, ForgotPassword, Login, Register, Agent } from "./pages";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ColorModeContextProvider>
           <CssBaseline />
@@ -55,21 +50,46 @@ function App() {
                 authProvider={authProvider}
                 resources={[
                   {
-                    name: "blog_posts",
-                    list: "/blog-posts",
-                    create: "/blog-posts/create",
-                    edit: "/blog-posts/edit/:id",
-                    show: "/blog-posts/show/:id",
+                    name: "home",
+                    list: "/",
+                    meta: {
+                      label: "Dashboard",
+                      icon: <DashboardOutlined />,
+                    },
+                  },
+                  {
+                    name: "property",
+                    list: "/property",
+                    icon: <VillaOutlined />,
                     meta: {
                       canDelete: true,
                     },
                   },
                   {
-                    name: "categories",
-                    list: "/categories",
-                    create: "/categories/create",
-                    edit: "/categories/edit/:id",
-                    show: "/categories/show/:id",
+                    name: "agent",
+                    icon: <PeopleAltOutlined />,
+                    meta: {
+                      canDelete: true,
+                    },
+                  },
+                  {
+                    name: "review",
+                    icon: <StarOutlineRounded />,
+                    meta: {
+                      canDelete: true,
+                    },
+                  },
+                  {
+                    name: "message",
+                    icon: <ChatBubbleOutline />,
+                    meta: {
+                      canDelete: true,
+                    },
+                  },
+                  {
+                    name: "my-profile",
+                    icon: <AccountCircleOutlined />,
+                    options: { label: 'My Profile'},
                     meta: {
                       canDelete: true,
                     },
@@ -95,22 +115,11 @@ function App() {
                       </Authenticated>
                     }
                   >
+                    <Route index element={<Home />} />
                     <Route
                       index
                       element={<NavigateToResource resource="blog_posts" />}
                     />
-                    <Route path="/blog-posts">
-                      <Route index element={<BlogPostList />} />
-                      <Route path="create" element={<BlogPostCreate />} />
-                      <Route path="edit/:id" element={<BlogPostEdit />} />
-                      <Route path="show/:id" element={<BlogPostShow />} />
-                    </Route>
-                    <Route path="/categories">
-                      <Route index element={<CategoryList />} />
-                      <Route path="create" element={<CategoryCreate />} />
-                      <Route path="edit/:id" element={<CategoryEdit />} />
-                      <Route path="show/:id" element={<CategoryShow />} />
-                    </Route>
                     <Route path="*" element={<ErrorComponent />} />
                   </Route>
                   <Route
