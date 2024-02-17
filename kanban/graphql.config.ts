9a7450c2e359d9279822a8286667a386ef788aa3
justin7251 <myjustinleung19@hotmail.com>
2024-02-17 10:53:30 +0000

    add graphql query

---
+++ kanban/graphql.config.ts
@@ -0,0 +1,47 @@
+import type { IGraphQLConfig } from 'graphql-config';
+
+const config: IGraphQLConfig = {
+    schema: 'https://api.crm.refine.dev/graphql',
+    extensions: {
+        codegen: {
+            // hooks are commands that are executed after a certain event
+            hooks: {
+              afterOneFileWrite: ["eslint --fix", "prettier --write"],
+            },
+            generates: {
+                // specify the output path of the generated types
+                "src/graphql/schema.types.ts": {
+                  plugins: ["typescript"],
+                  config: {
+                    skipTypename: true,
+                    enumsAsTypes: true,
+                    scalars: {
+                      DateTime: {
+                        input: "string",
+                        output: "string",
+                        format: "date-time",
+                      },
+                    },
+                  },
+                },
+                "src/graphql/types.ts": {
+                    // https://the-guild.dev/graphql/codegen/plugins/presets/import-types-preset
+                    preset: "import-types",
+                    documents: ["src/**/*.{ts,tsx}"],
+                    plugins: ["typescript-operations"],
+                    config: {
+                      skipTypename: true,
+                      enumsAsTypes: true,
+                      preResolveTypes: false,
+                      useTypeImports: true,
+                    },
+                    presetConfig: {
+                      typesPath: "./schema.types",
+                    },
+                },
+            },
+        },
+    }
+}
+
+export default config
