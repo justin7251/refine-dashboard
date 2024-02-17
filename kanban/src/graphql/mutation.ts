801ddc0e74ef3428e25d03d4d92f74e26eca88a0
justin725 <jleung@marketforce.com>
2024-02-17 10:53:30 +0000

    add graphql query

---
+++ kanban/src/graphql/mutation.ts
@@ -0,0 +1,67 @@
+import gql from "graphql-tag";
+
+// Mutation to update user
+export const UPDATE_USER_MUTATION = gql`
+  # The ! after the type means that it is required
+  mutation UpdateUser($input: UpdateOneUserInput!) {
+    # call the updateOneUser mutation with the input and pass the $input argument
+    # $variableName is a convention for GraphQL variables
+    updateOneUser(input: $input) {
+      id
+      name
+      avatarUrl
+      email
+      phone
+      jobTitle
+    }
+  }
+`;
+
+// Mutation to update task stage of a task
+export const UPDATE_TASK_STAGE_MUTATION = gql`
+  mutation UpdateTaskStage($input: UpdateOneTaskInput!) {
+    updateOneTask(input: $input) {
+      id
+    }
+  }
+`;
+
+// Mutation to create a new task
+export const CREATE_TASK_MUTATION = gql`
+  mutation CreateTask($input: CreateOneTaskInput!) {
+    createOneTask(input: $input) {
+      id
+      title
+      stage {
+        id
+        title
+      }
+    }
+  }
+`;
+
+// Mutation to update a task details
+export const UPDATE_TASK_MUTATION = gql`
+  mutation UpdateTask($input: UpdateOneTaskInput!) {
+    updateOneTask(input: $input) {
+      id
+      title
+      completed
+      description
+      dueDate
+      stage {
+        id
+        title
+      }
+      users {
+        id
+        name
+        avatarUrl
+      }
+      checklist {
+        title
+        checked
+      }
+    }
+  }
+`;
