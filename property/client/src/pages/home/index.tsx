b530b51d2e2a8ced8d12054f9dfc6a3f2e36e925
justin7251 <myjustinleung19@hotmail.com>
2025-01-26 21:12:56 +0000

    move position

--- property/client/src/pages/home/index.tsx
+++ property/client/src/pages/home/index.tsx
@@ -46,32 +46,6 @@ export const Home: React.FC = () => {
       <Typography fontSize={25} fontWeight={700} color="#11142D">
         Dashboard
       </Typography>
-      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
-        <PieChart
-            title="Properties for Sale"
-            value={ 684}
-            series={[75, 25]}
-            colors={["#275be8", "#c4e8ef"]}
-        />
-        <PieChart
-            title="Properties for Rent"
-            value={550}
-            series={[60, 40]}
-            colors={["#275be8", "#c4e8ef"]}
-        />
-        <PieChart
-            title="Total customers"
-            value={5684}
-            series={[75, 25]}
-            colors={["#275be8", "#c4e8ef"]}
-        />
-        <PieChart
-            title="Properties for Cities"
-            value={555}
-            series={[75, 25]}
-            colors={["#275be8", "#c4e8ef"]}
-        />
-      </Box>
 
       {/* UK Property Market Updates */}
       {marketUpdates && (
@@ -129,6 +103,33 @@ export const Home: React.FC = () => {
         </Paper>
       )}
 
+<Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart
+            title="Properties for Sale"
+            value={ 684}
+            series={[75, 25]}
+            colors={["#275be8", "#c4e8ef"]}
+        />
+        <PieChart
+            title="Properties for Rent"
+            value={550}
+            series={[60, 40]}
+            colors={["#275be8", "#c4e8ef"]}
+        />
+        <PieChart
+            title="Total customers"
+            value={5684}
+            series={[75, 25]}
+            colors={["#275be8", "#c4e8ef"]}
+        />
+        <PieChart
+            title="Properties for Cities"
+            value={555}
+            series={[75, 25]}
+            colors={["#275be8", "#c4e8ef"]}
+        />
+      </Box>
+      
       <Stack mt="25px" width="100%"
       direction={{xs: 'column', lg: 'row'}}>
         <TotalRevenue />
